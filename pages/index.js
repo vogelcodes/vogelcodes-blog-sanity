45f01d37c1ae9673be2d556094db469aaefdf32d
Daniel Vogel <danielcvt@gmail.com>
2021-02-15 17:11:22 -0300

    googleanalytics

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Index({ allPosts, preview }) {
     <>
       <Layout preview={preview}>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>VogelCodes - Serviços WEB</title>
         </Head>
         <Container>
           <Intro />
